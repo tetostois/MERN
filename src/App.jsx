daea5265fac1053fd39dbd30123f4b4599c6ddbc
KAMGA <tetakamgasteve@gmail.com>
2023-12-22 03:55:37 -0500

    install react js and tailwind css, create the route pages, created header component, create and run server

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignUp from "./pages/SignUp";
+import About from './pages/About';
+import SignIn from "./pages/SignIn";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element= {<Home />} />
+        <Route path="/sign-in" element= {<SignIn />} />
+        <Route path="/sign-up" element= {<SignUp />} />
+        <Route path="/profil" element= {<Profile />} />
+        <Route path="/about" element= {<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
